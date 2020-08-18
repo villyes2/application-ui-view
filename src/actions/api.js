2750aba21a13aaaf2bf41deed9a7319b7437a1c7
villyes2 <66767404+villyes2@users.noreply.github.com>
2020-08-18 13:43:38 +0530

    changes Done
    Action types changes done

---
+++ src/actions/api.js
@@ -0,0 +1,28 @@
+import { API, API_START, API_END, ACCESS_DENIED, API_ERROR } from "../actions/types";
+
+export const api = label => ({
+  type: API,
+  payload: label
+});
+
+export const apiStart = label => ({
+  type: API_START,
+  payload: label
+});
+
+export const apiEnd = label => ({
+  type: API_END,
+  payload: label
+});
+
+export const accessDenied = url => ({
+  type: ACCESS_DENIED,
+  payload: {
+    url
+  }
+});
+
+export const apiError = error => ({
+  type: API_ERROR,
+  error
+});

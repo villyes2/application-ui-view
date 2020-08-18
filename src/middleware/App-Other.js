2336da18e07f60e03ef023808e81506a4aba27d8
villyes2 <66767404+villyes2@users.noreply.github.com>
2020-08-18 16:45:59 +0530

    App-Other Created for Testing
    App-Other Created for Testing

---
+++ src/middleware/App-Other.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+//import { Route } from 'react-router-dom';
+import styled from "styled-components";
+import DataDetails from "../Components/DataDetails";
+//import PageDetails from "../components/PageDetails";
+import { fetchArticleDetails } from "../actions";
+import { Container, Row, Col } from 'reactstrap';
+import ModalForm from '../Components/Modals/Modal';
+import DataTable from '../Components/Tables/DataTable';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { CSVLink } from "react-csv"
+
+const StyledApp = styled.div`
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+
+  aside {
+    min-width: 35vh;
+    display: flex;
+    justify-content: flex-end;
+  }
+  main {
+    flex: 1 0 350px;
+    ${"" /* not responsive */} padding: 0 5rem;
+  }
+`;
+
+class App extends Component {
+  state = {
+    items: []
+  }
+
+  getItems(){
+    fetch('http://localhost:81/crud')
+      .then(response => response.json())
+      .then(items => this.setState({items}))
+      .catch(err => console.log(err))
+  }
+
+  addItemToState = (item) => {
+    this.setState(prevState => ({
+      items: [...prevState.items, item]
+    }))
+  }
+
+  updateState = (item) => {
+    const itemIndex = this.state.items.findIndex(data => data.id === item.id)
+    const newArray = [
+    // destructure all items from beginning to the indexed item
+      ...this.state.items.slice(0, itemIndex),
+    // add the updated item to the array
+      item,
+    // add the rest of the items to the array from the index after the replaced item
+      ...this.state.items.slice(itemIndex + 1)
+    ]
+    this.setState({ items: newArray })
+  }
+
+  deleteItemFromState = (id) => {
+    const updatedItems = this.state.items.filter(item => item.id !== id)
+    this.setState({ items: updatedItems })
+  }
+
+  componentDidMount(){
+    this.getItems()
+  }
+
+  render() {
+    return (
+      <Container className="App">
+        <Row>
+          <Col>
+            <h1 style={{margin: "20px 0"}}>UI APPLICATON</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <DataTable items={this.state.items} updateState={this.updateState} deleteItemFromState={this.deleteItemFromState} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <CSVLink
+              filename={"db.csv"}
+              color="primary"
+              style={{float: "left", marginRight: "10px"}}
+              className="btn btn-primary"
+              data={this.state.items}>
+              Download CSV
+            </CSVLink>
+            <ModalForm buttonLabel="Add Item" addItemToState={this.addItemToState}/>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default App-Other

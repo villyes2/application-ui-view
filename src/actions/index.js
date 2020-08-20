714b03f3d51afa349c2de7dcfc5af00feb1129b1
villyes2 <66767404+villyes2@users.noreply.github.com>
2020-08-20 12:24:52 +0530

    Actions Changed
    Actions Changed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,44 +1,9 @@
-import { SET_ARTICLE_DETAILS, API, FETCH_ARTICLE_DETAILS } from "./types";
-const proxyurl = "http://localhost:81/crud";
-export function fetchArticleDetails() {
-  return apiAction({
-    url: proxyurl+"http://localhost:81/crud",
-    onSuccess: setArticleDetails,
-    onFailure: () => console.log("Error occured loading Data Details"),
-    label: FETCH_ARTICLE_DETAILS
-  });
-}
+export const ADD_ARTICLE = "http://localhost:81/crud";
+export const ADD_USERS = "http://localhost:81/users";
+export const UPDATE_ARTICLE = "UPDATE_ARTICLE";
 
-function setArticleDetails(data) {
-  return {
-    type: SET_ARTICLE_DETAILS,
-    payload: data
-  };
-}
-
-function apiAction({
-  url = "",
-  method = "GET",
-  data = null,
-  mode = 'cors',
-  headers = {
-                'Access-Control-Allow-Origin':'*'
-            },
-  onSuccess = () => {},
-  onFailure = () => {},
-  label = "",
-  headersOverride = null
-}) {
-  return {
-    type: API,
-    payload: {
-      url,
-      method,
-      data,
-      onSuccess,
-      onFailure,
-      label,
-      headersOverride
-    }
-  };
-}
+export const API = "API";
+export const API_START = "API_START";
+export const API_END = "API_END";
+export const ACCESS_DENIED = "ACCESS_DENIED";
+export const API_ERROR = "API_ERROR";

753e37511be57f2f5b9f0d2f444ed79ecf082785
villyes2 <66767404+villyes2@users.noreply.github.com>
2020-08-20 12:13:34 +0530

    File Deleted
    D

--- actions/types.js
+++
@@ -1,8 +0,0 @@
-export const FETCH_ARTICLE_DETAILS = "FETCH_ARTICLE_DETAILS";
-export const SET_ARTICLE_DETAILS = "SET_ARTICLE_DETAILS";
-
-export const API = "API";
-export const API_START = "API_START";
-export const API_END = "API_END";
-export const ACCESS_DENIED = "ACCESS_DENIED";
-export const API_ERROR = "API_ERROR";

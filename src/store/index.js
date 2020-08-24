9c72633105a4b85f368245e01aa7e22fd0f52bcb
villyes2 <66767404+villyes2@users.noreply.github.com>
2020-08-24 12:05:18 +0530

    store created
    store created

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers";
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import apiMiddleware from "../middleware/api";
+const loggerMiddleware = createLogger();
 
-const store = createStore(rootReducer, applyMiddleware(apiMiddleware));
+const store = createStore(rootReducer, applyMiddleware(apiMiddleware, thunkMiddleware,
+        loggerMiddleware));
 window.store = store;
 export default store;
+
+

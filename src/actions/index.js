478c53ee47cc498c720728aa57bc6ff524adf5f1
villyes2 <66767404+villyes2@users.noreply.github.com>
2020-08-24 10:47:50 +0530

    user-actions, alert-actions added-
    user-actions, alert-actions added-

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,6 @@ export const API_START = "API_START";
 export const API_END = "API_END";
 export const ACCESS_DENIED = "ACCESS_DENIED";
 export const API_ERROR = "API_ERROR";
+
+export * from './user.actions';
+export * from './alert.actions';

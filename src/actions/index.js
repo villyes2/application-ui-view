7c464c937185735e0abb2de5d41b487ab3e1312b
villyes2 <66767404+villyes2@users.noreply.github.com>
2020-08-24 10:51:27 +0530

    actions added
    actions added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 export const ADD_ARTICLE = "http://localhost:81/crud";
+export const ADD_USERS = "http://localhost:81/users";
 export const UPDATE_ARTICLE = "UPDATE_ARTICLE";
 
 export const API = "API";
@@ -6,3 +7,6 @@ export const API_START = "API_START";
 export const API_END = "API_END";
 export const ACCESS_DENIED = "ACCESS_DENIED";
 export const API_ERROR = "API_ERROR";
+
+export * from './user.actions';
+export * from './alert.actions';
